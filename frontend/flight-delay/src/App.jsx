126170758c0608d63fb7f95a596575788e674278
thisisaditya17 <joshi.adi1734@gmail.com>
2025-09-09 14:45:42 +0800

    update to host on github pages

--- frontend/flight-delay/src/App.jsx
+++ frontend/flight-delay/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
     setLoading(true);
     
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://flight-delay-prediction-system.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
